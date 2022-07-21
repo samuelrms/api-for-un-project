a6de6e7f076099f5924d864146bf0e62d05e71a7
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-07-21 18:38:42 -0300

    Fix: authorization

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ app.use(express.json());
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin:
+    "http://localhost:3000" || "https://desafio-final-mod-3.herokuapp.com",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
