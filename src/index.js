573995104b975adee9209a236787b100f43d79b8
Samuel Ramos <samuelaoliveiraramos@gmail.com>
2022-07-21 19:11:27 -0300

    Fix

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ const { uuid } = require("uuidv4");
 
 app.use(express.json());
 
-// const cors = require("cors");
-// const corsOptions = {
-//   origin:
-//     "http://localhost:3000" || "https://desafio-final-mod-3.herokuapp.com",
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
+const cors = require("cors");
+const corsOptions = {
+  origin: "https://desafio-final-mod-3.herokuapp.com",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Cards path
 const endpoints_cards = require("./cards/index.json");
